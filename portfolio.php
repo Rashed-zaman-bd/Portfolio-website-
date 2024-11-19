1af3eb0b598a3ce2b9e986f1cbf65238fa37d58e
rashed-zaman <bangla.bd.rashed@gmail.com>
2024-11-19 12:14:45 +0600

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,57 @@
+<?php
+include 'header.php';
+?>
+
+<div class="page-header animate__animated animate__zoomIn">
+  <h2>Portfolio</h2>
+  <div class="devider"></div>
+  <p class="subtitle">work display</p>
+</div>
+
+<div class="container">
+  <div class="gallery">
+
+  <div class="small">
+      <a href="../img/img1.jpg" data-lightbox="models" data-title="e-commerce website development, www.bilasup.com">
+        <img src="../img/img1.jpg" alt="">
+       
+      </a>
+    </div>
+
+  <div class="small-2">
+      <a href="../img/logo.png" data-lightbox="models" data-title="logo design">
+        <img src="../img/logo.png" alt="">
+      </a>
+    </div>
+
+    
+
+    <div class="big">
+      <a href="../img/img2.jpg" data-lightbox="models" data-title="travel website development, www.flytraveltrip.com">
+        <img src="../img/img2.jpg" alt="" >    
+      </a>
+    </div>
+
+   
+    
+    <div class="logo">
+      <a href="../img/logo.gif" data-lightbox="models" data-title="animated logo design">
+        <img src="../img/logo.gif" alt="">
+      </a>
+    </div>
+
+    
+   
+  </div>
+</div>
+
+
+
+
+
+
+
+
+<?php
+include 'footer.php';
+?>
